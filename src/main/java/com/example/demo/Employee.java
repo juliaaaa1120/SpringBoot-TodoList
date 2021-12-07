af783ec7d767dececb377f8cbf04d937b1f8f174
Julia Ng <ngju@oocl.com>
2021-12-07 18:02:47 +0800

    [EmployeesAPIDemo][JuliaNg] - feat: first commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,47 @@
+//package com.example.demo;
+//
+//public class Employee {
+//    private final Integer id;
+//    private final String name;
+//    private Integer age;
+//    private final String gender;
+//    private Integer salary;
+//
+//
+//    public Employee(int id, String name, int age, String gender, int salary) {
+//        this.id = id;
+//        this.name = name;
+//        this.age = age;
+//        this.gender = gender;
+//        this.salary = salary;
+//    }
+//
+//    public Integer getId() {
+//        return this.id;
+//    }
+//
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public Integer getAge() {
+//        return this.age;
+//    }
+//
+//    public String getGender() {
+//        return this.gender;
+//    }
+//
+//    public Integer getSalary() {
+//        return this.salary;
+//    }
+//
+////    public void setAge(Integer age) {
+////        this.age = age;
+////    }
+////
+////    public void setSalary(Integer salary) {
+////        this.salary = salary;
+////    }
+//}
+//
---
+++ src/main/java/com/example/demo/NoEmployeeFoundException.java
@@ -0,0 +1,7 @@
+//package com.example.demo;
+//
+//public class NoEmployeeFoundException extends RuntimeException {
+//    public NoEmployeeFoundException() {
+//
+//    }
+//}
