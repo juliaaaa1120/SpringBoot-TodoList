3946ef0ca9da4598bdde41642f4f51876846f5d8
Julia Ng <ngju@oocl.com>
2021-12-07 18:06:55 +0800

    [EmployeesAPIDemo][JuliaNg] - feat: create GET employee list

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,52 +1,52 @@
-//package com.example.demo;
-//
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("employees")
-//public class EmployeeController {
-//    private final EmployeeRepository employeeRepository;
-//
-//    public EmployeeController(EmployeeRepository employeeRepository) {
-//        this.employeeRepository = employeeRepository;
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("employees")
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+//    @GetMapping("/{id}")
+//    public Employee getEmployeeById(@PathVariable Integer id) {
+//        return employeeRepository.findById(id);
 //    }
 //
-//    @GetMapping
-//    public List<Employee> getAllEmployees() {
-//        return employeeRepository.findAll();
+//    @GetMapping(params = {"gender"})
+//    public List<Employee> getEmployeesByGender(@RequestParam String gender) {
+//        return employeeRepository.findByGender(gender);
 //    }
 //
-////    @GetMapping("/{id}")
-////    public Employee getEmployeeById(@PathVariable Integer id) {
-////        return employeeRepository.findById(id);
-////    }
-////
-////    @GetMapping(params = {"gender"})
-////    public List<Employee> getEmployeesByGender(@RequestParam String gender) {
-////        return employeeRepository.findByGender(gender);
-////    }
-////
-////    @GetMapping(params = {"page", "pageSize"})
-////    public List<Employee> getEmployeesByPage(@RequestParam Integer page, @RequestParam Integer pageSize) {
-////        return employeeRepository.findByPage(page, pageSize);
-////    }
-////
-////    @PostMapping
-////    public Employee createEmployee(@RequestBody Employee employee) {
-////        return employeeRepository.create(employee);
-////    }
-////
-////    @PutMapping("/{id}")
-////    public Employee editEmployee(@PathVariable Integer id, @RequestBody Employee updatedEmployee) {
-////        Employee employee = employeeRepository.findById(id);
-////        if (employee.getAge() != null) {
-////            employee.setAge(updatedEmployee.getAge());
-////        }
-////        if (employee.getSalary() != null) {
-////            employee.setSalary(employee.getSalary());
-////        }
-////        return employeeRepository.save(id, updatedEmployee);
-////    }
-//}
+//    @GetMapping(params = {"page", "pageSize"})
+//    public List<Employee> getEmployeesByPage(@RequestParam Integer page, @RequestParam Integer pageSize) {
+//        return employeeRepository.findByPage(page, pageSize);
+//    }
+//
+//    @PostMapping
+//    public Employee createEmployee(@RequestBody Employee employee) {
+//        return employeeRepository.create(employee);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public Employee editEmployee(@PathVariable Integer id, @RequestBody Employee updatedEmployee) {
+//        Employee employee = employeeRepository.findById(id);
+//        if (employee.getAge() != null) {
+//            employee.setAge(updatedEmployee.getAge());
+//        }
+//        if (employee.getSalary() != null) {
+//            employee.setSalary(employee.getSalary());
+//        }
+//        return employeeRepository.save(id, updatedEmployee);
+//    }
+}
--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -1,62 +1,62 @@
-//package com.example.demo;
-//
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Repository
-//public class EmployeeRepository {
-//    private List<Employee> employees = new ArrayList<>();
-//
-//    public EmployeeRepository() {
-//        employees.add(new Employee(1, "Julia", 18, "F", 100000));
-//        employees.add(new Employee(2, "Jason", 18, "M", 100000));
-//        employees.add(new Employee(3, "Klaus", 18, "M", 100000));
-//        employees.add(new Employee(4, "Joanne", 18, "F", 100000));
-//        employees.add(new Employee(5, "John", 18, "M", 100000));
-//        employees.add(new Employee(6, "Johnson", 18, "M", 100000));
-//        employees.add(new Employee(7, "Nicole", 18, "F", 100000));
+package com.example.demo;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class EmployeeRepository {
+    private List<Employee> employees = new ArrayList<>();
+
+    public EmployeeRepository() {
+        employees.add(new Employee(1, "Julia", 18, "F", 100000));
+        employees.add(new Employee(2, "Jason", 18, "M", 100000));
+        employees.add(new Employee(3, "Klaus", 18, "M", 100000));
+        employees.add(new Employee(4, "Joanne", 18, "F", 100000));
+        employees.add(new Employee(5, "John", 18, "M", 100000));
+        employees.add(new Employee(6, "Johnson", 18, "M", 100000));
+        employees.add(new Employee(7, "Nicole", 18, "F", 100000));
+    }
+
+
+    public List<Employee> findAll() {
+        return employees;
+    }
+
+//    public Employee findById(Integer id) {
+//        return employees.stream()
+//                .filter(employee -> employee.getId().equals(id))
+//                .findFirst()
+//                .orElseThrow(NoEmployeeFoundException::new);
 //    }
 //
-//
-//    public List<Employee> findAll() {
-//        return employees;
+//    public List<Employee> findByGender(String gender) {
+//        return employees.stream()
+//                .filter(employee -> employee.getGender().equals(gender))
+//                .collect(Collectors.toList());
 //    }
 //
-////    public Employee findById(Integer id) {
-////        return employees.stream()
-////                .filter(employee -> employee.getId().equals(id))
-////                .findFirst()
-////                .orElseThrow(NoEmployeeFoundException::new);
-////    }
-////
-////    public List<Employee> findByGender(String gender) {
-////        return employees.stream()
-////                .filter(employee -> employee.getGender().equals(gender))
-////                .collect(Collectors.toList());
-////    }
-////
-////    public List<Employee> findByPage(Integer page, Integer pageSize) {
-////        return employees.stream()
-////                .skip((long) page * pageSize)
-////                .limit(pageSize)
-////                .collect(Collectors.toList());
-////    }
-////
-////    public Employee create(Employee employee) {
-////        Integer nextId = employees.stream()
-////                .mapToInt(Employee::getId)
-////                .max()
-////                .orElse(0) + 1;
-////        return new Employee(nextId, employee.getName(), employee.getAge(), employee.getGender(), employee.getSalary());
-////    }
-////
-////    public Employee save(Integer id, Employee updatedEmployee) {
-////        Employee employee = findById(id);
-////        employees.remove(employee);
-////        employees.add(updatedEmployee);
-////        return updatedEmployee;
-////    }
-//}
+//    public List<Employee> findByPage(Integer page, Integer pageSize) {
+//        return employees.stream()
+//                .skip((long) page * pageSize)
+//                .limit(pageSize)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public Employee create(Employee employee) {
+//        Integer nextId = employees.stream()
+//                .mapToInt(Employee::getId)
+//                .max()
+//                .orElse(0) + 1;
+//        return new Employee(nextId, employee.getName(), employee.getAge(), employee.getGender(), employee.getSalary());
+//    }
+//
+//    public Employee save(Integer id, Employee updatedEmployee) {
+//        Employee employee = findById(id);
+//        employees.remove(employee);
+//        employees.add(updatedEmployee);
+//        return updatedEmployee;
+//    }
+}
