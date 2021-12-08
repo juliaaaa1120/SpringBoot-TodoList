32840a7ea7d89c30df918adab263ab75918c4ea1
Julia Ng <ngju@oocl.com>
2021-12-08 17:58:39 +0800

    [APIDemo][JuliaNg] - feat: completed test case should_get_companies_by_page_when_perform_get_given_page_and_page_size()

--- src/test/java/com/afs/restapi/CompanyControllerTest.java
+++ src/test/java/com/afs/restapi/CompanyControllerTest.java
@@ -95,7 +95,7 @@ public class CompanyControllerTest {
     }
 
     @Test
-    void should_all_employees_in_company_when_perform_get_given_company_id_employees() throws Exception {
+    void should_get_all_employees_in_company_when_perform_get_given_company_id_employees() throws Exception {
         //given
         Company company = new Company(1, "OOCL", Arrays.asList(
                 new Employee(1, "Julia", 18, "Female", 100000),
@@ -119,36 +119,54 @@ public class CompanyControllerTest {
                 .andExpect(jsonPath("$[1].salary").value(100000));
         //then
     }
-//
-//    @Test
-//    void should_get_employees_by_page_when_perform_get_given_page_and_page_size() throws Exception {
-//        //given
-//        Employee employee1 = new Employee(1, "Julia", 18, "Female", 100000);
-//        companyRepository.create(employee1);
-//        Employee employee2 = new Employee(2, "Jason", 18, "Male", 100000);
-//        companyRepository.create(employee2);
-//        Employee employee3 = new Employee(3, "Gloria", 18, "Female", 100000);
-//        companyRepository.create(employee3);
-//        Employee employee4 = new Employee(4, "Johnson", 18, "Male", 100000);
-//        companyRepository.create(employee4);
-//        //when
-//        mockMvc.perform(MockMvcRequestBuilders.get("/employees")
-//                .param("page", "2")
-//                .param("pageSize", "2"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", hasSize(2)))
-//                .andExpect(jsonPath("$[0].id").value(3))
-//                .andExpect(jsonPath("$[0].name").value("Gloria"))
-//                .andExpect(jsonPath("$[0].age").value(18))
-//                .andExpect(jsonPath("$[0].gender").value("Female"))
-//                .andExpect(jsonPath("$[0].salary").value(100000))
-//                .andExpect(jsonPath("$[1].id").value(4))
-//                .andExpect(jsonPath("$[1].name").value("Johnson"))
-//                .andExpect(jsonPath("$[1].age").value(18))
-//                .andExpect(jsonPath("$[1].gender").value("Male"))
-//                .andExpect(jsonPath("$[1].salary").value(100000));
-//        //then
-//    }
+
+    @Test
+    void should_get_companies_by_page_when_perform_get_given_page_and_page_size() throws Exception {
+        //given
+        Company company1 = new Company(1, "OOCL", Arrays.asList(
+                new Employee(1, "Julia", 18, "Female", 100000),
+                new Employee(2, "Jason", 18, "Male", 100000),
+                new Employee(3, "Klaus", 18, "Male", 100000)
+        ));
+        companyRepository.create(company1);
+        Company company2 = new Company(2, "DHL", Arrays.asList(
+                new Employee(4, "Joanne", 18, "Female", 100000),
+                new Employee(5, "John", 18, "Male", 100000),
+                new Employee(6, "Johnson", 18, "Male", 100000),
+                new Employee(7, "Nicole", 18, "Female", 100000)
+        ));
+        companyRepository.create(company2);
+        Company company3 = new Company(3, "SF Express", Arrays.asList(
+                new Employee(8, "Gloria", 18, "Female", 100000)
+        ));
+        companyRepository.create(company3);
+        Company company4 = new Company(4, "Disney", Arrays.asList(
+                new Employee(9, "Koby", 18, "Male", 100000)
+        ));
+        companyRepository.create(company4);
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.get("/companies")
+                .param("page", "2")
+                .param("pageSize", "2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(3))
+                .andExpect(jsonPath("$[0].companyName").value("SF Express"))
+                .andExpect(jsonPath("$[0].employees[0].id").value(8))
+                .andExpect(jsonPath("$[0].employees[0].name").value("Gloria"))
+                .andExpect(jsonPath("$[0].employees[0].age").value(18))
+                .andExpect(jsonPath("$[0].employees[0].gender").value("Female"))
+                .andExpect(jsonPath("$[0].employees[0].salary").value(100000))
+                .andExpect(jsonPath("$[1].id").value(4))
+                .andExpect(jsonPath("$[1].companyName").value("Disney"))
+                .andExpect(jsonPath("$[1].employees[0].id").value(9))
+                .andExpect(jsonPath("$[1].employees[0].name").value("Koby"))
+                .andExpect(jsonPath("$[1].employees[0].age").value(18))
+                .andExpect(jsonPath("$[1].employees[0].gender").value("Male"))
+                .andExpect(jsonPath("$[1].employees[0].salary").value(100000));
+
+        //then
+    }
 //
 //    @Test
 //    void should_return_employee_when_perform_post_given_employee() throws Exception {
