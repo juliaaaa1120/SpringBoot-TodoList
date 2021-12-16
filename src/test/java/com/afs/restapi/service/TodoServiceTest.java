f39959807578a4a3a79f58c16e3c9e6f49325322
Julia Ng <ngju@oocl.com>
2021-12-16 22:28:48 +0800

    [SpringBootTodos][JuliaNg] - feat: completed TodoServiceTest should_return_todo_when_create_todo_given_todo()

--- src/test/java/com/afs/restapi/service/TodoServiceTest.java
+++ src/test/java/com/afs/restapi/service/TodoServiceTest.java
@@ -44,97 +44,25 @@ public class TodoServiceTest {
         assertEquals(todos.get(0).getText(), actual.get(0).getText());
         assertEquals(todos.get(0).getDone(), actual.get(0).getDone());
     }
-//
-//    @Test
-//    void should_return_employee_by_id_when_find_by_id_given_employee_id() {
-//        //given
-//        Todo todo1 = new Todo("1", "Julia", 22, "Female", "1", 100000);
-//        Todo todo2 = new Todo("2", "Jason", 22, "Male", "1", 100000);
-//        Todo todo3 = new Todo("3", "Joanne", 22, "Female", "1",100000);
-//
-//        given(mockTodoRepositoryInMongo.findById(todo3.getId()))
-//                .willReturn(java.util.Optional.of(todo3));
-//
-//        //when
-//        Todo actual = todoService.findById(todo3.getId());
-//
-//        //then
-//        assertEquals(todo3, actual);
-//        assertEquals(todo3.getId(), actual.getId());
-//        assertEquals(todo3.getName(), actual.getName());
-//        assertEquals(todo3.getAge(), actual.getAge());
-//        assertEquals(todo3.getGender(), actual.getGender());
-//        assertEquals(todo3.getCompanyId(), actual.getCompanyId());
-//        assertEquals(todo3.getSalary(), actual.getSalary());
-//    }
-//
-//    @Test
-//    void should_return_employees_by_gender_when_find_by_gender_given_employee_gender() {
-//        //given
-//        List<Todo> femaleTodos = new ArrayList<>();
-//        Todo todo1 = new Todo("1", "Julia", 22, "Female", "1", 100000);
-//        Todo todo2 = new Todo("2", "Jason", 22, "Male", "1", 100000);
-//        Todo todo3 = new Todo("3", "Joanne", 22, "Female", "1", 100000);
-//        femaleTodos.add(todo1);
-//        femaleTodos.add(todo3);
-//
-//        given(mockTodoRepositoryInMongo.findByGender("Female"))
-//                .willReturn(femaleTodos);
-//
-//        //when
-//        List<Todo> actual = todoService.findByGender("Female");
-//
-//        //then
-//        assertEquals(femaleTodos, actual);
-//        assertEquals(2, actual.size());
-//    }
-//
-//    @Test
-//    void should_return_employees_by_page_when_find_by_page_given_page_and_page_size() {
-//        //given
-//        List<Todo> todos = new ArrayList<>();
-//        Todo todo1 = new Todo("1", "Julia", 22, "Female", "1", 100000);
-//        Todo todo2 = new Todo("2", "Jason", 22, "Male", "1", 100000);
-//        Todo todo3 = new Todo("3", "Joanne", 22, "Female", "1", 100000);
-//        Todo todo4 = new Todo("3", "John", 22, "Male", "1", 100000);
-//        todos.add(todo1);
-//        todos.add(todo2);
-//        todos.add(todo3);
-//        todos.add(todo4);
-//
-//        PageImpl<Todo> returnedPage = new PageImpl<>(todos, PageRequest.of(2, 2), 1);
-//
-//        given(mockTodoRepositoryInMongo.findAll(any(PageRequest.class)))
-//                .willReturn(returnedPage);
-//
-//        //when
-//        List<Todo> actual = todoService.findByPage(2,2);
-//
-//        //then
-//        assertEquals(returnedPage.getContent(), actual);
-//    }
-//
-//    @Test
-//    void should_return_employee_when_create_employee_given_employee() {
-//        //given
-//        Todo todo = new Todo("1", "Julia", 22, "Female", "1", 100000);
-//
-//        given(mockTodoRepositoryInMongo.insert(todo))
-//                .willReturn(todo);
-//
-//        //when
-//        Todo actual = todoService.create(todo);
-//
-//        //then
-//        assertEquals(todo, actual);
-//        assertEquals(todo.getId(), actual.getId());
-//        assertEquals(todo.getName(), actual.getName());
-//        assertEquals(todo.getAge(), actual.getAge());
-//        assertEquals(todo.getGender(), actual.getGender());
-//        assertEquals(todo.getCompanyId(), actual.getCompanyId());
-//        assertEquals(todo.getSalary(), actual.getSalary());
-//    }
-//
+
+    @Test
+    void should_return_todo_when_create_todo_given_todo() {
+        //given
+        Todo todo = new Todo("1", "Have fun Christmas party", false);
+
+        given(mockTodoRepositoryInMongo.insert(todo))
+                .willReturn(todo);
+
+        //when
+        Todo actual = todoService.create(todo);
+
+        //then
+        assertEquals(todo, actual);
+        assertEquals(todo.getId(), actual.getId());
+        assertEquals(todo.getText(), actual.getText());
+        assertEquals(todo.getDone(), actual.getDone());
+    }
+
 //    @Test
 //    void should_return_updated_employee_when_edit_employee_given_updated_employee() {
 //        //given
